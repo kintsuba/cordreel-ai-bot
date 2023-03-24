4a0b112c219bc9d875f2a62cc24619b4992e7e95
Kintsuba <bnxth107@gmail.com>
2023-03-24 18:42:48 +0900

    feat: creat a function to react

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import * as Misskey from "misskey-js";
+import WebSocket from "ws";
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from "dotenv";
+import { react } from "./functions";
+
+dotenv.config();
+
+const main = async () => {
+  if (
+    !process.env.MISSKEY_HOST ||
+    !process.env.MISSKEY_TOKEN ||
+    !process.env.OPENAI_API_KEY
+  ) {
+    console.error(
+      "MISSKEY_HOST, MISSKEY_TOKEN and OPENAI_API_KEY must be included in the .env file."
+    );
+    process.exit(1);
+  }
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const cli = new Misskey.api.APIClient({
+    origin: process.env.MISSKEY_HOST,
+    credential: process.env.MISSKEY_TOKEN,
+  });
+
+  const stream = new Misskey.Stream(
+    process.env.MISSKEY_HOST,
+    {
+      token: process.env.MISSKEY_TOKEN,
+    },
+    WebSocket
+  );
+
+  const LtlChannel = stream.useChannel("hybridTimeline");
+  LtlChannel.on("note", async (note) => {
+    const result = await react(cli, openai, note);
+  });
+};
+
+main();
---
+++ src/functions/react.ts
@@ -0,0 +1,37 @@
+import { APIClient } from "misskey-js/built/api";
+import { Note } from "misskey-js/built/entities";
+import { OpenAIApi } from "openai";
+
+export const react = async (
+  cli: APIClient,
+  openai: OpenAIApi,
+  note: Note
+): Promise<string> => {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content:
+          "Reply to the following social networking post with just one emoji",
+      },
+      {
+        role: "user",
+        content: note.text ?? "",
+      },
+    ],
+  });
+
+  if (
+    response.statusText !== "OK" ||
+    !response.data.choices[0].message?.content
+  )
+    return "NG";
+
+  cli.request("notes/reactions/create", {
+    noteId: note.id,
+    reaction: response.data.choices[0].message?.content,
+  });
+
+  return "OK";
+};
